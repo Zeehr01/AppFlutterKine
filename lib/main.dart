16838843cfe9d900732d15333594be23e5d9282c
Zahra <biadyzahra12@gmail.com>
2021-01-13 11:24:02 +0100

    Adding a user model and authentification service firt try

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.grey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: PatientOverView(),
+      home: SignIn(),
     );
   }
 }
