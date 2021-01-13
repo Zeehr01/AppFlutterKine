61bbe2109d2169c77a7c0167707c27c8d9d6abd6
Zahra <biadyzahra12@gmail.com>
2021-01-13 02:17:11 +0100

    ajout de la fonctionalité mes programmes dans la vue patient

--- lib/views/patientOverView.dart
+++ lib/views/patientOverView.dart
@@ -11,6 +11,37 @@ class PatientOverView extends StatefulWidget {
 class _PatientOverView extends State<PatientOverView> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: appBarMain(context));
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: Column(children: [
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: 15,
+            ),
+            const Icon(
+              Icons.article_rounded,
+              color: Color(0xFFA5D6A7),
+              size: 40.0,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              "Mes Programmes :",
+              style: TextStyle(
+                color: Color(0xFF329C9D),
+                fontSize: 20,
+              ),
+              textAlign: TextAlign.left,
+            ),
+          ],
+        ),
+      ]),
+    );
   }
 }
